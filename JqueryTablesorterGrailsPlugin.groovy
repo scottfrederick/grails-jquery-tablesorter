0cfbece8da3932da75dfdd1d0de5bdcc09d980a5
Scott Frederick <scottyfred@gmail.com>
2011-09-12 19:19:21 -0500

    Initial commit.

---
+++ JqueryTablesorterGrailsPlugin.groovy
@@ -0,0 +1,28 @@
+class JqueryTablesorterGrailsPlugin {
+    // the plugin version
+    def version = "2.0.5"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "1.3.7 > *"
+    // the other plugins this plugin depends on
+    def dependsOn = [jquery:'1.3.2 > *']
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+    ]
+
+    // TODO Fill in these fields
+    def author = "Scott Frederick"
+    def authorEmail = "scottyfred@gmail.com"
+    def title = "JQuery TableSorter Plugin"
+    def description = '''\\
+Integrates the JQuery TableSorter plugin to allow in-place sorting of HTML tables.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/jquery-tablesorter"
+
+    def doWithSpring = {
+    }
+
+    def doWithApplicationContext = { applicationContext ->
+    }
+}
---
+++ grails-app/conf/UrlMappings.groovy
@@ -0,0 +1,10 @@
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?" {
+            constraints {
+                // apply constraints here
+            }
+        }
+    }
+}
