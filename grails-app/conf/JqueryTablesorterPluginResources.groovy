8ca05dc0d3b1cfb4ad3851f622e28da192d2c87c
Scott Frederick <scottyfred@gmail.com>
2011-09-13 21:54:18 -0500

    Fixed resources definition.

--- grails-app/conf/JqueryTablesorterPluginResources.groovy
+++ grails-app/conf/JqueryTablesorterPluginResources.groovy
@@ -4,15 +4,14 @@ def jqtsver = plugin.instance.JQUERY_TABLESORTER_VERSION
 
 modules = {
     'jquery-tablesorter-global' {
-        resource id: 'js', url: [plugin: 'jqueryTablesorter', dir: 'js', file: "grails-tablesorter.js"],
-                nominify: true, disposition: 'head'
+        resource id: 'js', url: [plugin: 'jqueryTablesorter', dir: 'js', file: "grails-tablesorter.js"]
     }
 
     'jquery-tablesorter' {
         dependsOn 'jquery', 'jquery-tablesorter-global'
 
         resource id: 'js', url: [plugin: 'jqueryTablesorter', dir: 'js/jquery-tablesorter', file: "jquery.tablesorter-${jqtsver}.min.js"],
-                nominify: true, disposition: 'head'
+                nominify: true
     }
 
     'jquery-tablesorter-dev' {
